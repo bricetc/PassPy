6e39778ce76a6c6395250d95d9e3e80c84caaef1
Joseph TC <bricetchappi@gmail.com>
2025-01-13 03:06:40 +0100

    Hey the Procfile, gunicorn and app main changes

--- app.py
+++ app.py
@@ -292,5 +292,7 @@ def cookies_policy():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.2", port=7000, debug=True)
+    # app.run(host="127.0.0.2", port=7000, debug=True)
+    port = int(os.environ.get('PORT', 7000))
+    app.run(host='0.0.0.0', port=port)
 
